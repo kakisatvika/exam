b377010474263aba514a7406069217c2be53aa85
kakisatvika <160473559+kakisatvika@users.noreply.github.com>
2025-10-28 10:39:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-import flask
-from flask import Flask, render_template, request
-app = Flask(__name__)
+from flask import Flask,render_template, request
+app=Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('greeting.html', name="User")
-@app.route('/submit', methods=['POST'])
+    return render_template("form.html")
+@app.route('/submit',methods=['POST'])
 def submit():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    number = request.form.get('number')
-    age = request.form.get('age')
-    return render_template('greeting.html', name=name, email=email, number=number, age=age)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    username=request.form.get('username')
+    email=request.form.get('email')
+    year=request.form.get('year')
+    phone=request.form.get('phone')
+    return render_template('result.html',name=username,email=email,year=year,phone=phone)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5001,debug=True)
