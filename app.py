9d356bbdf033158c3610af7bbcfd4535c1aaf582
kakisatvika <160473559+kakisatvika@users.noreply.github.com>
2025-10-28 10:32:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def submit():
     age = request.form.get('age')
     return render_template('greeting.html', name=name, email=email, number=number, age=age)
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001)
+    app.run(host='0.0.0.0', port=5001)
